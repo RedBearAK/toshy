b3ee76db6a6124bff6b4b347be9460cefaadcafe
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-05-08 11:13:26 -0800

    Shell launchers for desktop apps (venv)

---
+++ scripts/bin/toshy-gui.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+# Start Toshy GUI app after activating venv
+source "$HOME/.config/toshy/.venv/bin/activate"
+
+/usr/bin/python3 "$HOME/.config/toshy/toshy_gui.py"
+
+deactivate
