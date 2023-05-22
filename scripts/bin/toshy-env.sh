54f41d4c08bff61eeb77cabf6f375b41502fd0cb
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-05-22 02:20:38 -0800

    Guard 'if' blocks, block running as root, etc.

--- scripts/bin/toshy-env.sh
+++ scripts/bin/toshy-env.sh
@@ -3,4 +3,17 @@
 
 # Run the Toshy environment module to show what it sees
 
+# Check if the script is being run as root
+if [[ $EUID -eq 0 ]]; then
+    echo "This script must not be run as root"
+    exit 1
+fi
+
+# Check if $USER and $HOME environment variables are not empty
+if [[ -z $USER ]] || [[ -z $HOME ]]; then
+    echo "\$USER and/or \$HOME environment variables are not set. We need them."
+    exit 1
+fi
+
+
 $(which python3) "$HOME/.config/toshy/lib/env.py"
--- scripts/bin/toshy-gui.sh
+++ scripts/bin/toshy-gui.sh
@@ -2,6 +2,20 @@
 
 
 # Start Toshy GUI app after activating venv
+
+# Check if the script is being run as root
+if [[ $EUID -eq 0 ]]; then
+    echo "This script must not be run as root"
+    exit 1
+fi
+
+# Check if $USER and $HOME environment variables are not empty
+if [[ -z $USER ]] || [[ -z $HOME ]]; then
+    echo "\$USER and/or \$HOME environment variables are not set. We need them."
+    exit 1
+fi
+
+
 # shellcheck disable=SC1091
 source "$HOME/.config/toshy/.venv/bin/activate"
 
