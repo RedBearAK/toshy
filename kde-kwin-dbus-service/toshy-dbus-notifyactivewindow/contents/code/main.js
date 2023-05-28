8dadd0bb5891b88c67fef7cc741ea7f190436739
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-05-28 14:04:55 -0800

    Remove console logging lines from KWin script

--- kde-kwin-dbus-service/toshy-dbus-notifyactivewindow/contents/code/main.js
+++ kde-kwin-dbus-service/toshy-dbus-notifyactivewindow/contents/code/main.js
@@ -1,9 +1,4 @@
 workspace.clientActivated.connect(function(client){
-    console.log("client: " + client);
-    console.log("caption: " + client.caption);
-    console.log("resourceClass: " + client.resourceClass);
-    console.log("resourceName: " + client.resourceName);
-
     callDBus(
         "org.toshy.Toshy",
         "/org/toshy/Toshy",
