33170c3a9353f61a40162e291f26544b788ba544
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-05-28 16:09:45 -0800

    Get initial window info update before focus change

--- kde-kwin-dbus-service/toshy-dbus-notifyactivewindow/contents/code/main.js
+++ kde-kwin-dbus-service/toshy-dbus-notifyactivewindow/contents/code/main.js
@@ -1,4 +1,18 @@
-workspace.clientActivated.connect(function(client){
+// Original form of the KWin script:
+
+// workspace.clientActivated.connect(function(client){
+//     callDBus(
+//         "org.toshy.Toshy",
+//         "/org/toshy/Toshy",
+//         "org.toshy.Toshy",
+//         "NotifyActiveWindow",
+//         "caption" in client ? client.caption : "",
+//         "resourceClass" in client ? client.resourceClass : "",
+//         "resourceName" in client ? client.resourceName : ""
+//     );
+// });
+
+function notifyActiveWindow(client){
     callDBus(
         "org.toshy.Toshy",
         "/org/toshy/Toshy",
@@ -8,4 +22,12 @@ workspace.clientActivated.connect(function(client){
         "resourceClass" in client ? client.resourceClass : "",
         "resourceName" in client ? client.resourceName : ""
     );
+}
+
+if (workspace.activeClient) {
+    notifyActiveWindow(workspace.activeClient);
+}
+
+workspace.clientActivated.connect(function(client){
+    notifyActiveWindow(client);
 });
