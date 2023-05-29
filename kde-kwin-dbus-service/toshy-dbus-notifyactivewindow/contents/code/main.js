7e7a06113a1d234fef3d6dc296899e63c0e7205d
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-05-28 21:49:31 -0800

    Stop trying to switch windows in KWin script

--- kde-kwin-dbus-service/toshy-dbus-notifyactivewindow/contents/code/main.js
+++ kde-kwin-dbus-service/toshy-dbus-notifyactivewindow/contents/code/main.js
@@ -24,28 +24,12 @@ function notifyActiveWindow(client){
     );
 }
 
-function switchWindows() {
-    var originalClient = workspace.activeClient;
-    var allClients = workspace.clientList();
-
-    // Index of the currently active client
-    var originalIndex = allClients.indexOf(originalClient);
-
-    // Calculate the index of the next client in the list
-    var nextIndex = (originalIndex + 1) % allClients.length;
-
-    // Activate the next client (equivalent to Alt+Tab)
-    workspace.activateClient(allClients[nextIndex]);
-
-    // Re-activate the original client (equivalent to Alt+Tab)
-    workspace.activateClient(originalClient);
-}
-
-// Set a delay of 1000ms (1 second) before executing the script
-setTimeout(switchWindows, 1000);
-
 if (workspace.activeClient) {
     notifyActiveWindow(workspace.activeClient);
+}
+
+if (client) {
+    notifyActiveWindow(client)
 }
 
 workspace.clientActivated.connect(function(client){
