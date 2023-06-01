fa1ed59fb1a5a5b4b1db0b7c160a8c0e0035d656
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-06-01 00:34:28 -0800

    Tweak output new lines

--- scripts/bin/toshy-venv.sh
+++ scripts/bin/toshy-venv.sh
@@ -18,10 +18,10 @@ if [[ -z "$VIRTUAL_ENV" ]]; then
     # If the VIRTUAL_ENV variable is empty, the virtual environment is not active.
     echo -e "\n To activate the Toshy Python virtual environment, run this command: \n"
     echo -e "\t\t\t source toshy-venv \n"
-    echo -e " (Ignore the text above if you've already run the command as shown.) \n\n"
+    echo -e " (Ignore the text above if you've already run the command as shown.) \n"
 else
     # If the VIRTUAL_ENV variable is not empty, the virtual environment is active.
-    echo -e "\n The Toshy Python virtual environment is ACTIVE in this shell already. \n"
+    echo -e "\n The Toshy Python virtual environment is ACTIVE in this shell already.\n"
     return 0
 fi
 
