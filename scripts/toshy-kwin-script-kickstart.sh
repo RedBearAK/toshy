68645a743da09ca0a046174e37ce8ab2d0f557d7
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-06-12 23:45:44 -0800

    Script to create a KWin event (new window)

---
+++ scripts/toshy-kwin-script-kickstart.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Script to create a KWin event that will get the KWin script to start working
+
+# Check if the script is being run as root
+if [[ $EUID -eq 0 ]]; then
+    echo "This script must not be run as root"
+    exit 1
+fi
+
+# # Check if $USER and $HOME environment variables are not empty
+# if [[ -z $USER ]] || [[ -z $HOME ]]; then
+#     echo "\$USER and/or \$HOME environment variables are not set. We need them."
+#     exit 1
+# fi
+
+sleep 3
+
+message="Kickstarting the Toshy KWin script..."
+timeout_s=2
+
+
+if command -v zenity &> /dev/null; then
+    zenity --info --text="${message}" --timeout=${timeout_s}
+elif command -v xmessage &> /dev/null; then
+    xmessage "${message}" -timeout ${timeout_s}
+else
+    echo "ERROR: The 'zenity' and 'xmessage' commands are not available."
+    echo "ERROR: Toshy cannot kickstart the KWin script."
+fi
