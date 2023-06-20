43e0d15db3d67b6d8e3ea5a8e817c95b0046e649
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-06-19 22:34:40 -0800

    Log window context vars from KWin script

--- kde-kwin-dbus-service/toshy-dbus-notifyactivewindow/contents/code/main.js
+++ kde-kwin-dbus-service/toshy-dbus-notifyactivewindow/contents/code/main.js
@@ -12,15 +12,36 @@
 //     );
 // });
 
+// function notifyActiveWindow(client){
+//     callDBus(
+//         "org.toshy.Toshy",
+//         "/org/toshy/Toshy",
+//         "org.toshy.Toshy",
+//         "NotifyActiveWindow",
+//         "caption" in client ? client.caption : "UNDEF",
+//         "resourceClass" in client ? client.resourceClass : "UNDEF",
+//         "resourceName" in client ? client.resourceName : "UNDEF"
+//     );
+// }
+
 function notifyActiveWindow(client){
+    var caption = client.hasOwnProperty('caption') ? client.caption : "UNDEF";
+    var resourceClass = client.hasOwnProperty('resourceClass') ? client.resourceClass : "UNDEF";
+    var resourceName = client.hasOwnProperty('resourceName') ? client.resourceName : "UNDEF";
+    
+    console.log("Calling D-Bus with the following variables:");
+    console.log("Caption: " + caption);
+    console.log("Resource Class: " + resourceClass);
+    console.log("Resource Name: " + resourceName);
+    
     callDBus(
         "org.toshy.Toshy",
         "/org/toshy/Toshy",
         "org.toshy.Toshy",
         "NotifyActiveWindow",
-        "caption" in client ? client.caption : "UNDEF",
-        "resourceClass" in client ? client.resourceClass : "UNDEF",
-        "resourceName" in client ? client.resourceName : "UNDEF"
+        caption,
+        resourceClass,
+        resourceName
     );
 }
 
