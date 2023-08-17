14433c473ae8a5b4c53c61294fb5bd4c44694d7d
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-08-16 18:17:00 -0800

    Bump config version

--- default-toshy-config/toshy_config.py
+++ default-toshy-config/toshy_config.py
@@ -75,7 +75,7 @@ icon_file_inverse   = os.path.join(assets_path, "toshy_app_icon_rainbow_inverse.
 # Toshy config file
 TOSHY_PART      = 'config'   # CUSTOMIZE TO SPECIFIC TOSHY COMPONENT! (gui, tray, config)
 TOSHY_PART_NAME = 'Toshy Config file'
-APP_VERSION     = '2023.0604'
+APP_VERSION     = '2023.0816'
 
 # Settings object used to tweak preferences "live" between gui, tray and config.
 cnfg = Settings(current_folder_path)
