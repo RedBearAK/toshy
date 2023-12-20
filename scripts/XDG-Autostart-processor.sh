36550b70baf74954201df91489e63006cd3772a6
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-12-19 20:41:14 -0900

    Fix autostart processor script

--- scripts/XDG-Autostart-processor.sh
+++ scripts/XDG-Autostart-processor.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-# script to process and launch items in the XDG Autostart user location (~/.config/autostart)
+# Script to process and launch items in the XDG Autostart user location (~/.config/autostart)
 
 for entry in ~/.config/autostart/*; do
     if [[ -f "$entry" ]]; then
         # Check if the file is a desktop entry
         if [[ $(file --mime-type -b "$entry") == "application/x-desktop" ]]; then
             # Extract the Exec command from the desktop entry and execute it
-            grep '^Exec=' "$entry" | sed 's/^Exec=//' | sh
-        else
-            # Execute the file directly (useful for scripts but may not be valid in the XDG spec)
-            sh "$entry"
+            cmd=$(grep '^Exec=' "$entry" | head -n 1 | sed 's/^Exec=//' | cut -d ' ' -f 1)
+            # Execute the command using nohup
+            # shellcheck disable=SC2086
+            nohup $cmd > /dev/null 2>&1 &
+        elif [[ -x "$entry" ]]; then
+            # If the file is executable, run it directly
+            nohup "$entry" > /dev/null 2>&1 &
         fi
     fi
 done
