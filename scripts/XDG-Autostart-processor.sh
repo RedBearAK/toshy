231d58981b5145465819c4295dbec4b950c97859
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-12-19 21:36:45 -0900

    Stop using mime types, tweak logging

--- scripts/XDG-Autostart-processor.sh
+++ scripts/XDG-Autostart-processor.sh
@@ -6,26 +6,24 @@
 # Function to echo and log a message
 log_and_echo() {
     local message="$1"
-    echo "$message" | tee >(logger)
+    echo "$message" | tee >(logger -p user.info)
 }
 
 
 log_and_echo "Processing user XDG Autostart folder contents..."
 
 
-for entry in ~/.config/autostart/*; do
+for entry in ~/.config/autostart/*.desktop; do
     if [[ -f "$entry" ]]; then
-        # Check if the file is a desktop entry
-        if [[ $(file --mime-type -b "$entry") == "application/x-desktop" ]]; then
-            # Extract the Exec command from the desktop entry and execute it
-            cmd=$(grep '^Exec=' "$entry" | head -n 1 | sed 's/^Exec=//' | sed 's/%.//g')
-            log_and_echo "Launching file: '$entry'"
-            # shellcheck disable=SC2086
-            $cmd > /dev/null 2>&1 &
-        elif [[ -x "$entry" ]]; then
-            # If the file is executable, run it directly
-            "$entry" > /dev/null 2>&1 &
-        fi
+        # Extract the Exec command from the desktop entry and execute it
+        cmd=$(grep '^Exec=' "$entry" | head -n 1 | sed 's/^Exec=//' | sed 's/%.//g')
+        log_and_echo "Launching desktop entry file: '$entry'"
+        # shellcheck disable=SC2086
+        $cmd > /dev/null 2>&1 &
+    elif [[ -x "$entry" ]]; then
+        # If the file is executable, run it directly
+        log_and_echo "Launching executable file: '$entry'"
+        "$entry" > /dev/null 2>&1 &
     fi
 done
 
