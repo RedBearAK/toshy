75ddb036b9ce08243f2a6f539f26df1056c798d4
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2024-03-22 22:33:54 -0800

    Bump barebones config file version

--- default-toshy-config/toshy_config_barebones.py
+++ default-toshy-config/toshy_config_barebones.py
@@ -75,7 +75,7 @@ icon_file_inverse   = os.path.join(assets_path, "toshy_app_icon_rainbow_inverse.
 # Toshy config file
 TOSHY_PART      = 'config'   # CUSTOMIZE TO SPECIFIC TOSHY COMPONENT! (gui, tray, config)
 TOSHY_PART_NAME = 'Toshy Barebones Config'
-APP_VERSION     = '2024.0202'
+APP_VERSION     = '2024.0322'
 
 # Settings object used to tweak preferences "live" between gui, tray and config.
 cnfg = Settings(current_folder_path)
