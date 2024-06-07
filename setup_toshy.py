5654b83e15a397471b50077af77a3e3d2ec20b3e
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2024-06-07 13:39:10 -0800

    Use a dual-color attention prompt

--- setup_toshy.py
+++ setup_toshy.py
@@ -399,7 +399,12 @@ def call_attn_to_pwd_prompt_if_sudo_tkt_exp():
         # sudo ticket not valid, requires a password, so get user attention
         print()
         print(fancy_str('  ----------------------------------------  ', 'blue', bold=True))
-        print(fancy_str('  -- SUDO PASSWORD REQUIRED TO CONTINUE --  ', 'blue', bold=True))
+        # print(fancy_str('  -- SUDO PASSWORD REQUIRED TO CONTINUE --  ', 'blue', bold=True))
+        print(
+            fancy_str('  -- ', 'blue', bold=True) +
+            fancy_str('SUDO PASSWORD REQUIRED TO CONTINUE', 'red', bold=True) +
+            fancy_str(' --  ', 'blue', bold=True)
+        )
         print(fancy_str('  ----------------------------------------  ', 'blue', bold=True))
         print()
 
