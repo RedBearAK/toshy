ca60e116f882095b27b644d90eebe2839c33d5f0
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2024-06-25 00:42:26 -0800

    Fix Solus package list (xkbregistry)

--- setup_toshy.py
+++ setup_toshy.py
@@ -757,7 +757,8 @@ pkg_groups_map: Dict[str, List[str]] = {
                             "evtest",
                             "gcc", "git",
                             "libayatana-appindicator-devel", "libgirepository-devel", "libnotify",
-                                "libxkbcommon-devel", "libxkbregistry-devel",
+                                "libxkbcommon-devel",
+                                # "libxkbregistry-devel" not available or needed on Void
                             "pkg-config", "python3-dbus", "python3-devel", "python3-pip",
                                 "python3-pkgconfig", "python3-tkinter",
                             "wayland-devel", "wget",
