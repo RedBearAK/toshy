0bccae10a3a823cd49b3a3fbf0fb549b39c81c69
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2024-07-09 13:57:34 -0800

    Have shell script also check/install SCL repos

--- prep_centos_before_setup.sh
+++ prep_centos_before_setup.sh
@@ -22,43 +22,75 @@ echo "Updating CentOS repos to use the CentOS Vault (if necessary)..."
 # Define the repository directory
 repo_dir="/etc/yum.repos.d"
 
-# Updating CentOS repos to use the CentOS Vault only if necessary
-echo "Checking and updating CentOS repository configurations..."
 
-# Reference commands:
-# sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
-# sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
-# sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
+check_and_update_repos() {
+    # Updating CentOS repos to use the CentOS Vault only if necessary
+    echo "Checking and updating CentOS repository configurations..."
 
-if grep -q -e 'mirror.centos.org' -e '^#.*baseurl=http' -e '^mirrorlist=http' "${repo_dir}"/*.repo; then
-    echo "Updates are necessary, modifying repository files..."
+    # Reference commands:
+    # sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
+    # sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
+    # sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
 
-    # Update mirror.centos.org to vault.centos.org
-    sudo sed -i 's/mirror.centos.org/vault.centos.org/g' "${repo_dir}"/*.repo
+    if grep -q -e 'mirror.centos.org' -e '^#.*baseurl=http' -e '^mirrorlist=http' "${repo_dir}"/*.repo; then
+        echo "Updates are necessary, modifying repository files..."
 
-    # Uncomment baseurl
-    sudo sed -i 's/^#.*baseurl=http/baseurl=http/g' "${repo_dir}"/*.repo
+        # Update mirror.centos.org to vault.centos.org
+        sudo sed -i 's/mirror.centos.org/vault.centos.org/g' "${repo_dir}"/*.repo
 
-    # Comment out mirrorlist
-    sudo sed -i 's/^mirrorlist=http/#mirrorlist=http/g' "${repo_dir}"/*.repo
+        # Uncomment baseurl
+        sudo sed -i 's/^#.*baseurl=http/baseurl=http/g' "${repo_dir}"/*.repo
 
-    echo "Repository files have been updated."
-else
-    echo "No updates are necessary."
-fi
+        # Comment out mirrorlist
+        sudo sed -i 's/^mirrorlist=http/#mirrorlist=http/g' "${repo_dir}"/*.repo
 
-# Clearing and refreshing YUM cache
-if command -v yum &> /dev/null
-then
-    echo "Refreshing YUM cache..."
-    sudo yum clean all
-    sudo yum makecache
-    echo "YUM cache has been refreshed."
-fi
+        echo "Repository files have been updated."
+    else
+        echo "No updates are necessary."
+    fi
+
+    # Clearing and refreshing YUM cache
+    if command -v yum &> /dev/null
+    then
+        echo "Refreshing YUM cache..."
+        sudo yum clean all
+        sudo yum makecache
+        echo "YUM cache has been refreshed."
+    fi
+}
+
+# Call once before trying to install SCL repos
+check_and_update_repos
+
+
+# Check and install SCL repos if they are not present (for CentOS 7)
+check_and_install_scl_repos() {
+    local scl_repo_files=("/etc/yum.repos.d/CentOS-SCLo-scl.repo" "/etc/yum.repos.d/CentOS-SCLo-scl-rh.repo")
+    local scl_repo_missing=false
+
+    for repo_file in "${scl_repo_files[@]}"; do
+        if [ ! -f "$repo_file" ]; then
+            scl_repo_missing=true
+            break
+        fi
+    done
+
+    if [ "$scl_repo_missing" = true ]; then
+        echo "SCL repositories are missing, installing..."
+        sudo yum install -y centos-release-scl
+        update_centos_repos_to_vault  # Call update function again if we install new repos
+    else
+        echo "SCL repositories are already installed."
+    fi
+}
+
 
 if $centos_7; then
 
-    echo "Detected CentOS 7. Checking for Python3 and DNF..."
+    echo "Detected CentOS 7. Checking for SCL repos, Python3 and DNF..."
+
+    # Install the SCL repos if necessary (checks and updates repos AGAIN, if necesssary)
+    check_and_install_scl_repos
 
     # Install python3 if not present
     if ! command -v python3 &> /dev/null; then
