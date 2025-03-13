a7f7408933d03fec71b1c2248b37d182ef8186b9
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2025-03-13 15:00:22 -0800

    Ver bump for Chimera icon change

--- toshy_tray.py
+++ toshy_tray.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__version__ = '20250227'
+__version__ = '20250313'
 
 # Indicator tray icon menu app for Toshy, using pygobject/gi
 TOSHY_PART      = 'tray'   # CUSTOMIZE TO SPECIFIC TOSHY COMPONENT! (gui, tray, config)
