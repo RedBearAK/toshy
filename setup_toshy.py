5516c835b3627d049af3c8c4fb9c76ac47a6b3bf
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2025-03-17 03:34:01 -0800

    Bump setup version

--- setup_toshy.py
+++ setup_toshy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = '20250313'                        # CLI option "--version" will print this out.
+__version__ = '20250317'                        # CLI option "--version" will print this out.
 
 import os
 os.environ['PYTHONDONTWRITEBYTECODE'] = '1'     # prevent this script from creating cache files
