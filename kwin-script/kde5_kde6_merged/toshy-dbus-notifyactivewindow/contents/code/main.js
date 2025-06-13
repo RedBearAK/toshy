621d7074ea66e52cc276f7fff56dae63e332f919
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2025-06-12 19:39:30 -0800

    Force debugMode to be true

--- kwin-script/kde5_kde6_merged/toshy-dbus-notifyactivewindow/contents/code/main.js
+++ kwin-script/kde5_kde6_merged/toshy-dbus-notifyactivewindow/contents/code/main.js
@@ -4,11 +4,15 @@ KWin Script Toshy D-Bus Notify Active Window - Direct Signal Approach
 GNU General Public License v3.0
 */
 
-const debugMode = readConfig("debugMode", true);
+// const debugMode = readConfig("debugMode", true);
+const debugMode = true
+
 function debug(...args) {
     if (debugMode) { console.debug("toshy-dbus-notifyactivewindow:", ...args); }
 }
-debug("initializing direct signal approach");
+
+debug("FORCED DEBUG MODE - bypassing readConfig");
+debug("initializing toshy-dbus-notifyactivewindow");
 
 // Detect KDE version
 const isKDE6 = typeof workspace.windowList === 'function';
